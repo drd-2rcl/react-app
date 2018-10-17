01bd9b3ba326df18ba93f4192d835e4f1d0818e6
drd-2rcl <drd.rclrd@gmail.com>
2018-10-17 18:36:06 -0300

    component product ok

--- src/component/product.js
+++ src/component/product.js
@@ -18,6 +18,23 @@ class Product extends Component {
     }
 
     render() {
-        
+        if (!this.state.loading) {
+            return (
+                <div className="product">
+                    <div className="product__information">
+                        <h2 className="Product-title">{this.state.product.name}</h2>
+                        <img src={`https://localhost:1337/${this.state.product.image.url}`} />
+                        <BuyButton {...this.state} />
+                    </div>
+                    <div className="product__description">
+                        {this.state.product.description}
+                    </div>
+                </div>
+            );
+        }
+
+        return (<h2>Waiting for API...</h2>);
     }
 }
+
+export default Product;
